5de947f321da1950cb9bc459574d4a51d132b536
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-12-01 23:15:10 +0200

    Route for creating a sub

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -11,3 +11,16 @@ router.get('/', async (req,res)=> {
         res.status(500).json({message: error.message})
     }
 })
+
+//Creating one
+router.post('/', async (req,res)=> {
+    const subscriber = new Subscriber({
+        name: req.body.name
+    })
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).send(newSubscriber)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
